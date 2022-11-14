0b973d0c800e1b8e65ff02bb57d2d2b8ea037c69
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-14 20:20:33 +0530

    Understanding commonJs module

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const http = require("http");
 
+const Circle = require("./circle");
+const circle = new Circle();
+console.log(circle.area(10));
+console.log(circle.circumference(10));
+
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello NodeJs",
-    })
-  );
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "application/json" });
+//   res.end(
+//     JSON.stringify({
+//       data: "Hello NodeJs",
+//     })
+//   );
+// });
 
-server.listen(port, () => {
-  console.log(`server running on port: ${port}📡`);
-});
+// server.listen(port, () => {
+//   console.log(`server running on port: ${port}📡`);
+// });
---
+++ circle.js
@@ -0,0 +1,18 @@
+class Circle {
+  area(r) {
+    return Math.PI * r * r;
+  }
+  circumference(r) {
+    return 2 * Math.PI * r;
+  }
+}
+
+// exports.area = function (r) {
+//   return Math.PI * r * r;
+// };
+
+// exports.circumference = function (r) {
+//   return 2 * Math.pi * r;
+// };
+
+module.exports = Circle;
