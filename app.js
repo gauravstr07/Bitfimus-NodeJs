a3f3eabe5df5e05257ef9b664ce381de97227c82
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-14 21:48:55 +0530

    Write Large file

--- app.js
+++ app.js
@@ -13,15 +13,28 @@ const server = http.createServer((req, res) => {
     });
   } else if (req.url === "/about") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    fs.readFile("page/about.html", (err, data) => {
+    fs.readFile("page/about.html", "utf-8", (err, data) => {
       if (err) throw Error;
       res.write(data);
       res.end();
     });
+  } else if (req.url === "/create-file") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const data = "<h1>This is test file</h1>";
+    for (let i = 0; i < 10; i++) {
+      fs.appendFile("temp/test.html", data, (err) => {
+        if (err) throw err;
+      });
+    }
+    res.write("file is created");
+    res.end();
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
-    res.write("<h1>Page not found</h1>");
-    res.end();
+    fs.readFile("page/404.html", "utf-8", (err, data) => {
+      if (err) throw Error;
+      res.write(data);
+      res.end();
+    });
   }
 });
 
