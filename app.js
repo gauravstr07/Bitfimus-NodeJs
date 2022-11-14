65e1bd86f71d3b0e212be05ab64812a48ab15572
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-14 21:05:47 +0530

    Show HTML content

--- app.js
+++ app.js
@@ -4,28 +4,19 @@ const port = 5000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "Hello World..!",
-      })
-    );
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Home Page</h1>");
+    res.end();
   }
 
   if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "About Page",
-      })
-    );
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>About Page</h1>");
+    res.end();
   } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "Page Not Found",
-      })
-    );
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h1>Page not found</h1>");
+    res.end();
   }
 });
 
