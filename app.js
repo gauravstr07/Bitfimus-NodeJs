2c83fc816919aa6fa6092f4bb64f73ecca85da26
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-14 21:26:16 +0530

    File System Module Read-File

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const port = 5000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
+    fs.readFile("page/home.html", "utf-8", (err, data) => {
+      if (err) throw Error;
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/about") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>Home Page</h1>");
-    res.end();
-  }
-
-  if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>About Page</h1>");
-    res.end();
+    fs.readFile("page/about.html", (err, data) => {
+      if (err) throw Error;
+      res.write(data);
+      res.end();
+    });
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.write("<h1>Page not found</h1>");
