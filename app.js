525f800c292d5b7cfe39332f9b0ea30b91fc5124
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-14 19:59:04 +0530

    Create first server

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const http = require("http");
 
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello NodeJs",
+    })
+  );
+});
+
+server.listen(port, () => {
+  console.log(`server running on port: ${port}📡`);
+});
