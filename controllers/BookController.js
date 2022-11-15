48aeef52ef867fd9f4b8340186e7407dbe6c6040
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-15 16:10:10 +0530

    Adding Controllers

---
+++ controllers/BookController.js
@@ -0,0 +1,35 @@
+const connect = require("../database/db");
+const { ObjectId } = require("mongodb");
+
+exports.index = async (req, res) => {
+  const db = await connect();
+  const books = await db.collection("book").find().toArray();
+  res.status(200).json(books);
+};
+
+exports.store = async (req, res) => {
+  const db = await connect();
+  await db.collection("book").insertOne(req.body);
+  res.status(201).json(req.body);
+};
+
+exports.show = async (req, res) => {
+  const _id = ObjectId(req.params.id);
+  const db = await connect();
+  const book = await db.collection("book").find({ _id }).toArray();
+  res.json(book);
+};
+
+exports.update = async (req, res) => {
+  const _id = ObjectId(req.params.id);
+  const db = await connect();
+  await db.collection("book").updateOne({ _id }, { $set: req.body });
+  res.send(`update book: ${req.params.id}`);
+};
+
+exports.delete = async (req, res) => {
+  const _id = ObjectId(req.params.id);
+  const db = await connect();
+  await db.collection("book").deleteOne({ _id });
+  res.status(204).json();
+};
--- routes/book.js
+++ routes/book.js
@@ -1,31 +1,13 @@
 const express = require("express");
 const bookRouter = express.Router();
-const connect = require("../database/db");
+const BookController = require("../controllers/BookController");
 
-bookRouter
-  .route("/")
-  .get(async (req, res) => {
-    const db = await connect();
-    const books = await db.collection("book").find().toArray();
-    res.json(books);
-  })
-  .post(async (req, res) => {
-    const db = await connect();
-
-    await db.collection("book").insertOne(req.body);
-    res.json(req.body);
-  });
+bookRouter.route("/").get(BookController.index).post(BookController.store);
 
 bookRouter
   .route("/:id")
-  .get((req, res) => {
-    res.send(`single book of id: ${req.params.id}`);
-  })
-  .patch((req, res) => {
-    res.send(`update book: ${req.params.id}`);
-  })
-  .delete((req, res) => {
-    res.send(`delted book: ${req.params.id}`);
-  });
+  .get(BookController.show)
+  .patch(BookController.update)
+  .delete(BookController.delete);
 
 module.exports = bookRouter;
