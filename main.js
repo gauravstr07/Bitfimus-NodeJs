3f2162283d8666499849736b70dd59734a2def84
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-15 11:36:39 +0530

    NodeJs Event Emitter

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+const EventEmitter = require("events");
 const express = require("express");
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on("test-event", (data) => {
+  console.log(data);
+});
 
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
+  myEmitter.emit("test-event", { name: "Gaurav" });
   res.send("Hello World");
 });
 
