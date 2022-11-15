a0433a64efddc82a267946c9349d78860ca8ebca
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-15 13:06:38 +0530

    expressjs server html file

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const bookRouter = require("./book");
+const path = require("path");
 const router = express.Router();
 
 router.use("/book", bookRouter);
 
 router.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, "/../page/home.html"), { name: "Gaurav" });
 });
 
 router.all("/*", (req, res) => {
-  res.send("page not found");
+  res.sendFile(path.join(__dirname, "/../page/404.html"));
 });
 
 module.exports = router;
